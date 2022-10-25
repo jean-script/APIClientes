6d74371ce64199b33b5d42b17d4238de068124cf
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2022-10-25 18:19:27 -0300

    mensagens de erro

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -11,7 +11,7 @@ class ClienteSerializer(serializers.ModelSerializer):
     def validate(self, data):
         
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':"Número do CPF inválido"})
+            raise serializers.ValidationError({'cpf':"Número do CPF inválido (Caso queira testar gere um no site: https://www.4devs.com.br/gerador_de_cpf)"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':" Não inclua números neste campo"})
         if not rg_valido(data['rg']):
