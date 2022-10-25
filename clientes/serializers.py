f2d956f6b0921fb3c91051b50fedbc081473f224
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2022-10-25 08:56:50 -0300

    Validando todos os campos

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from rest_framework import serializers
 from clientes.models import Cliente
 
@@ -6,3 +7,21 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
     
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
+        return cpf
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Não inclua números neste campo")
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve ter 9 dígitos")
+        return rg
+    
+    def validate(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("O celular deve ter no mínimo 11 dígitos")
+        return celular
