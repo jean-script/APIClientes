263f9ccab2aabd75bf7c1fbcf7ed51795a60f759
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2022-10-26 20:56:34 -0300

    mudando nome rg para ra

--- clientes/models.py
+++ clientes/models.py
@@ -4,7 +4,7 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(blank=False, max_length=30, )
     cpf = models.CharField(max_length=11, unique=True)
-    rg = models.CharField(max_length=9)
+    ra = models.CharField(max_length=9)
     celular = models.CharField(max_length=14)
     ativo = models.BooleanField()
 
