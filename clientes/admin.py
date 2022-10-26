69532dcc0f699860a186f4787e3931f12939ed8a
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2022-10-26 20:59:55 -0300

    atualizando campos

--- clientes/admin.py
+++ clientes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from clientes.models import Cliente
 
 class Clientes(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'email','cpf', 'rg', 'celular', 'ativo')
+    list_display = ('id', 'nome', 'email','cpf', 'ra', 'celular', 'ativo')
     list_display_links = ('id', 'nome')
     search_fields = ('nome','cpf',)
     list_filter = ('ativo',)
